0bba5bb56a91de96ca0ce28335f6bd0a5ca8c383
Andrew William Seward <awswrd@gmail.com>
2016-03-09 21:16:11 -0800

    Set up nupkg publishing
    All the actual work here done by @mglodack. I mostly just copied stuff
    over here.

--- ._fake/config.fsx
+++ ._fake/config.fsx
@@ -1,8 +1,9 @@
-namespace FSharpVersionUtils.Fake.Config
+namespace FSharpUtils.Fake.Config
 
 #r @"./packages/FAKE/tools/FakeLib.dll"
 
 open Fake
+open Fake.EnvironmentHelper
 open System.IO
 
 module Common =
@@ -17,3 +18,19 @@ module Build =
   let TestAssemblies = !! "tests/**/*.Tests.dll" -- "**/obj/**/*.Tests.dll"
   let DotNetVersion = "4.5"
   let MSBuildArtifacts = !! "**/bin/**.*" ++ "**/obj/**/*.*"
+
+module Nuget =
+  let ApiEnvVar = "DAT_NET_NUGET_API_KEY"
+  let ApiKey = environVar ApiEnvVar
+  let PackageDirName = "nupkgs"
+
+module Release =
+  let Items = !! "**/bin/Release/*"
+  let Nuspec = "FsharpAssemblyInfoUtils.nuspec"
+
+  let Version = "0.1.0-alpha"
+  let Project = "FSharp.AssemblyVersion.Utils"
+  let Authors = [ "Andrew Seward"; "Mathew Glodack" ]
+  let Description = "AssemblyInfo file versioning utilities"
+  let OutputPath = Nuget.PackageDirName
+  let WorkingDir = "bin"
