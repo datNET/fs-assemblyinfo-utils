671c05437069bbd385ebee8b8982d4ae906cefd0
Andrew William Seward <awswrd@gmail.com>
2016-04-24 12:58:12 -0700

    Add an initial test
    This is mostly to make sure we aren't royally screwing up line endings

--- tests/FSharpAssemblyInfoUtils.Tests/assemblyInfoVersionTests.fs
+++ tests/FSharpAssemblyInfoUtils.Tests/assemblyInfoVersionTests.fs
@@ -186,3 +186,54 @@ let ``parse AssemblyInformationalVersion (F#)`` () =
 let ``throw an exception if AssemblyInformationalVersion missing (F#)`` () =
   (fun() -> ParseInformationalVersionString withoutFsInformationalVersion |> ignore)
   |> should throw typeof<System.Exception>
+
+let assemblyInfoCsUpdatedContent =
+    @"
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle(""ExampleTitle"")]
+[assembly: AssemblyDescription(""ExampleDescription"")]
+[assembly: AssemblyCompany(""ExampleCompany"")]
+[assembly: AssemblyProduct(""ExampleProduct"")]
+[assembly: AssemblyCopyright(""Copyright ©  2014"")]
+
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid(""60fcbd4d-0b5a-4daf-bce8-a14c97cac246"")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion(""1.0.*"")]
+[assembly: AssemblyVersion(""UPDATED"")]
+[assembly: AssemblyFileVersion(""1.0.0.0"")]
+[assembly: AssemblyInformationalVersion(""1.0.0"")]
+    "
+
+// Egh this is stuff that's going to be changing so this test is kind of bogus for now
+[<Test>]
+let ``doesn't mess up whitespace when setting Attribute values`` () =
+  let updated = SetAssemblyVersion assemblyInfoCsContent "UPDATED"
+  let asString = String.Join(Environment.NewLine, updated)
+
+  System.IO.File.WriteAllText ("C:\\Users\\Andrew\\Desktop\\wtf.txt", asString)
+
+  asString
+  |> should equal assemblyInfoCsUpdatedContent
+
+  ignore()
